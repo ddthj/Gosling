518194013dd1d827c161446c9ac584495faca82d
ddthj <theddthj@gmail.com>
2018-11-22 14:52:22 -0500

    Experimental Gosling

---
+++ Experimental/Experimental.py
@@ -0,0 +1,189 @@
+import math
+import time
+from rlbot.agents.base_agent import  SimpleControllerState
+from Util import *
+
+class seven:
+    def __init__(self):
+        self.expired= False
+        self.jumping = False
+        self.time = time.time()
+    def execute(self,agent):
+        if self.jumping == False and agent.me.grounded:
+            self.jumping = True
+            test = dpp(agent.ball.location,agent.ball.velocity,agent.me.location,agent.me.velocity)
+            eta =  math.sqrt(abs((agent.ball.location - agent.me.location).data[2] - agent.me.location.data[2])/500) * 1.1
+            eta += math.sqrt(abs((agent.ball.location - agent.me.location).data[0] - agent.me.location.data[0])/500)
+            eta += math.sqrt(abs((agent.ball.location - agent.me.location).data[1] - agent.me.location.data[1])/500)
+            self.time = time.time() + eta
+            target = Vector3([0,0,0])
+        else:
+            time_remain = cap(self.time - time.time(),-2,10)
+            target = future(agent.ball, time_remain)
+            relative = target - agent.me.location
+            if time_remain > -1.9:
+                x = relative.data[0] / time_remain
+                y = relative.data[1] / time_remain
+                z = (relative.data[2]+(time_remain*325)) / time_remain
+                target = Vector3([x,y,z])
+                agent.renderer.begin_rendering()
+                agent.renderer.draw_line_3d(agent.me.location.data, targetFuture(agent.me.location,agent.me.velocity,time_remain).data, agent.renderer.create_color(255,255,190,0))
+                agent.renderer.draw_rect_3d(targetFuture(agent.me.location,agent.me.velocity,time_remain).data, 10,10, True, agent.renderer.create_color(255,255,190,0))
+                agent.renderer.draw_line_3d(agent.me.location.data, target.data, agent.renderer.create_color(255,0,190,255))
+                agent.renderer.draw_rect_3d(target.data, 10,10, True, agent.renderer.create_color(255,0,190,255))
+                agent.renderer.end_rendering()
+            else:
+                self.expired = True
+                target = agent.me.velocity
+                self.jumping = False
+        return deltaC(agent,target)
+
+def deltaC(agent, target):
+    c = SimpleControllerState()
+    target = target - agent.me.velocity
+    target_local = toLocal(agent.me.location + target,agent.me)
+    angle_to_target = math.atan2(target_local.data[1],target_local.data[0])
+    pitch_to_target = math.atan2(target_local.data[2],target_local.data[0])  
+    
+    if agent.me.grounded:
+        if agent.jt + 1.5 > time.time():
+            c.jump = True        
+        else:
+            c.jump = False
+            agent.jt = time.time()
+    else:
+        if agent.jt > time.time() - 1.5:
+            c.jump = True
+        else:
+            c.jump = False
+        c.yaw = steerPD(angle_to_target,-agent.me.rvelocity.data[1]/5)
+        c.pitch = steerPD(pitch_to_target,agent.me.rvelocity.data[0]/4)
+        if target.magnitude() > 30:
+            c.boost = True
+        if abs(pitch_to_target) + abs(angle_to_target) > 0.45:
+            c.boost = False
+
+    return c
+        
+
+
+def sevenC(agent,target,speed):
+    c = SimpleControllerState()
+
+    #target.data[2] = target.data[2]-90#futureZ(target.data[2],agent.ball.velocity.data[2], 0.2)
+    
+    angle = angle2(target,agent.me.location)
+    x_speed = (math.cos(angle) * speed*1.1) - (agent.me.velocity.data[0])
+    y_speed = (math.sin(angle) * speed*1.1) - (agent.me.velocity.data[1])
+    #print(x_speed,y_speed)
+    
+    correction_vector = Vector3([x_speed/2300,y_speed/2300,speed/2300])
+
+    target_local = toLocal(agent.me.location + correction_vector, agent.me)
+    angle_to_target = math.atan2(target_local.data[1],target_local.data[0])
+    pitch_to_target = math.atan2(target_local.data[2],target_local.data[0])  
+    
+    if agent.me.grounded:
+        if agent.jt + 1.5 > time.time():
+            c.jump = True        
+        else:
+            c.jump = False
+            agent.jt = time.time()
+    else:
+        if agent.jt > time.time() - 1.5:
+            c.jump = True
+        else:
+            c.jump = False
+        c.throttle,c.boost = altPD(target.data[2]-agent.me.location.data[2],agent.me.velocity.data[2])
+        c.yaw = steerPD(angle_to_target,-agent.me.rvelocity.data[1]/5)
+        c.pitch = steerPD(pitch_to_target,agent.me.rvelocity.data[0]/4)
+        if abs(pitch_to_target) + abs(angle_to_target) < 0.5:
+            two = target
+            loctwo = toLocal(two,agent.me)
+            to_two = math.atan2(loctwo.data[1],loctwo.data[2])
+            c.roll = steerPD(to_two,agent.me.rvelocity.data[2]/3)
+        if abs(pitch_to_target) + abs(angle_to_target) > 0.45:
+            c.boost = False
+
+    return c
+
+class six():
+    def __init__(self):
+        self.expired = False
+    def execute(self,agent):
+        adjust = agent.me.location + (agent.ball.location - agent.me.location).normalize().div(.0009)
+        #spacing = 150
+        #index = [agent.index]
+        '''
+        team = 1
+        x = 1
+        for car in agent.players:
+            if car.team == agent.team:
+                adjust += car.location
+                x+=1
+                team += 1
+                index.append(car.index)
+            else:
+                adjust += car.location.div(0.25)
+                x+=4                          
+        adjust = adjust.div(x)
+        if team == 4:
+            index.sort()
+            if index.index(agent.index) == 0:
+                adjust += Vector3([spacing,spacing,0])
+            if index.index(agent.index) == 1:
+                adjust += Vector3([spacing,-spacing,0])
+            if index.index(agent.index) == 2:
+                adjust += Vector3([-spacing,spacing,0])
+            if index.index(agent.index) == 3:
+                adjust += Vector3([-spacing,-spacing,0])
+        '''
+        '''
+        leftPost = Vector3([-sign(agent.team)*700 , 5100*sign(agent.team), 500])
+        for car in agent.players:
+            if car.team == agent.team:
+                index.append(car.index)
+        index.sort()
+        adjust += Vector3([spacing*index.index(agent.index),0,0])
+        '''
+        v = agent.me.velocity.data
+        target = Vector3([adjust.data[0],adjust.data[1],adjust.data[2]])
+        opposite = Vector3([-v[0],-v[1],0])#Vector3([agent.me.location.data[0],agent.me.location.data[1],1000])
+        target += opposite.div(1)#agent.me.velocity.magnitude())  
+        
+        speed = 2300
+        return sixC(agent,target,speed)
+
+    
+def sixC(agent,target,speed):
+    controller_state = SimpleControllerState()
+    target_relative = target.location - agent.me.location
+    target_vel_relative = target.velocity - agent.me.velocity
+    target_local = toLocal(target,agent.me)
+    agent_local = toLocal(agent.me.velocity,agent.me)
+    angle_to_target = math.atan2(target_local.data[1],target_local.data[0])
+    now = time.time()
+    if agent.me.grounded:
+        agent_speed = velocity2D(agent.me)
+        controller_state.steer = steer(angle_to_target)
+        controller_state.throttle, controller_state.boost = throttle(speed, agent_speed)
+        if target_local.data[2] > target_local.data[0]*3 and now > agent.jt + 1:
+            controller_state.jump = True
+            agent.jt = now
+        return controller_state
+    else:       
+        agent_speed = velocity2D(agent.me)
+        pitch_to_target = math.atan2(target_local.data[2],target_local.data[0])
+        controller_state.jump = False
+        controller_state.yaw = steerPD(angle_to_target,-agent.me.rvelocity.data[1]/5)
+        controller_state.pitch = steerPD(pitch_to_target,agent.me.rvelocity.data[0]/4)
+        controller_state.throttle, controller_state.boost = altPD(target_relative.data[2],agent.me.velocity.data[2])
+        if abs(pitch_to_target) > 0.4:
+            controller_state.boost = False
+        if abs(pitch_to_target) + abs(angle_to_target) < 0.5:
+            two = agent.ball.location
+            loctwo = toLocal(two,agent.me)
+            to_two = math.atan2(loctwo.data[1],loctwo.data[2])
+            controller_state.roll = steerPD(to_two,agent.me.rvelocity.data[2]/4)
+            
+        return controller_state   
---
+++ Experimental/Gosling.py
@@ -0,0 +1,126 @@
+import math
+import time
+from Util import *
+from States import *
+from Experimental import *
+import random
+
+from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
+from rlbot.utils.structures.game_data_struct import GameTickPacket
+
+from rlbot.utils.game_state_util import GameState, BallState, CarState, Physics, Vector3 as vector3, Rotator
+
+'''
+Gosling by ddthj
+Episode 5 Code
+
+*now with comments!*
+'''
+def rd(x):
+    return random.randint(-x,x)
+
+class Gosling(BaseAgent):
+
+    def initialize_agent(self):
+        self.me = obj()
+        self.ball = obj()
+        self.players = [] #holds other players in match
+        self.start = time.time()
+
+        self.state = seven()
+        self.controller = calcController
+        self.jt = time.time()
+
+    def checkState(self):
+        '''
+        try:
+            if self.players[0].location.data[2]>100:
+                print("reset")
+                car_state = CarState(boost_amount = 100, physics=Physics(velocity=vector3(0,0,0), location=vector3(0,1000,20),rotation=vector3(0,-1,0)))
+                ball_state = BallState(Physics(location=vector3(0,0,0),velocity=vector3(0,0,1000),angular_velocity=vector3(0,0,0)))
+                game_state = GameState(ball = ball_state,cars={self.index: car_state})
+                self.set_game_state(game_state)
+                self.jt = time.time() - 2
+                self.state = seven()
+        except:
+            pass
+        '''   
+        if self.state.expired or self.players[0].location.data[2]>100:
+            print("reset")
+            car_state = CarState(physics=Physics(location=vector3(0,0,20),velocity = vector3(0,0,0),rotation = vector3(0,rd(3),0)))
+            ball_state = BallState(Physics(location=vector3(rd(1500),0,rd(200)+500),velocity=vector3(rd(500),rd(500),rd(200)+1000),angular_velocity=vector3(0,0,0)))
+            game_state = GameState(ball = ball_state,cars={self.index: car_state})
+            self.set_game_state(game_state)
+            self.jt = time.time() - 2
+            self.state = seven()
+            '''
+            if calcShot().available(self) == True and self.ball.location.compare(Vector3([0,0,0])) > 1:
+                self.state = calcShot()
+            elif quickShot().available(self) == True:
+                self.state = quickShot()
+            elif wait().available(self) == True:
+                self.state = wait()
+            else:
+                self.state = wait()
+        '''
+    
+
+    def get_output(self, game: GameTickPacket) -> SimpleControllerState:
+        self.preprocess(game)
+        self.checkState()
+        return self.state.execute(self)
+
+    def preprocess(self,game):
+        self.players = []
+        car = game.game_cars[self.index]
+        self.me.location.data = [car.physics.location.x, car.physics.location.y, car.physics.location.z]
+        self.me.velocity.data = [car.physics.velocity.x, car.physics.velocity.y, car.physics.velocity.z]
+        self.me.rotation.data = [car.physics.rotation.pitch, car.physics.rotation.yaw, car.physics.rotation.roll]
+
+        temp = Vector3([car.physics.angular_velocity.x, car.physics.angular_velocity.y, car.physics.angular_velocity.z])
+        
+        self.me.matrix = rotator_to_matrix(self.me)
+        self.me.rvelocity.data = [temp*self.me.matrix[1],temp*self.me.matrix[2],temp*self.me.matrix[0]]
+        
+        self.me.boost = car.boost
+        self.me.grounded = car.has_wheel_contact
+
+        ball = game.game_ball.physics
+        self.ball.location.data = [ball.location.x, ball.location.y, ball.location.z]
+        self.ball.velocity.data = [ball.velocity.x, ball.velocity.y, ball.velocity.z]
+        self.ball.rotation.data = [ball.rotation.pitch, ball.rotation.yaw, ball.rotation.roll]
+        self.ball.rvelocity.data = [ball.angular_velocity.x, ball.angular_velocity.y, ball.angular_velocity.z]
+        
+
+        self.ball.local_location = to_local(self.ball,self.me)
+
+        #collects info for all other cars in match, updates objects in self.players accordingly
+        for i in range(game.num_cars):
+            if i != self.index:
+                car = game.game_cars[i]
+                temp = obj()
+                temp.index = i
+                temp.team = car.team
+                temp.location.data = [car.physics.location.x, car.physics.location.y, car.physics.location.z]
+                temp.velocity.data = [car.physics.velocity.x, car.physics.velocity.y, car.physics.velocity.z]
+                temp.rotation.data = [car.physics.rotation.pitch, car.physics.rotation.yaw, car.physics.rotation.roll]
+                #temps = Vector3([car.physics.angular_velocity.x, car.physics.angular_velocity.y, car.physics.angular_velocity.z])
+                #temp.matrix = rotator_to_matrix(temp)
+                #temp.rvelocity.data = [temps*temp.matrix[1],temps*temp.matrix[2],temps*temp.matrix[0]]
+                temp.boost = car.boost
+                flag = False
+                for item in self.players:
+                    if item.index == i:
+                        item = temp
+                        flag = True
+                        break
+                if not flag:
+                    self.players.append(temp)
+
+                
+
+            
+                
+
+                
+
